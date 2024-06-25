b6276f9e5c8a46cec63a3334792073aa8543c7b2
Hunter Shepherd <hshepherdmail@gmail.com>
2024-06-25 15:00:07 -0400

    Update service.py
    Co-authored-by: datadog-devsecops-ws[bot] <173838385+datadog-devsecops-ws[bot]@users.noreply.github.com>

--- service.py
+++ service.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 db_connection = db.connect_database()
 
 @app.route("/api/product/hash", methods=["GET"])
-def getProductHash():
+def get_product_hash():
     import hashlib
     products = db.get_products(db_connection, 100, 0)
     product_id = int(request.args.get('pid', 0))
